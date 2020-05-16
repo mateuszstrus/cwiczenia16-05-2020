87a9e5cd8839866215bf17254e7bab8e59e12b2f
mateuszstrus <mati1508@op.pl>
2020-05-16 13:54:32 +0200

    4 dodanie

--- cwiczenia/src/Firma.java
+++ cwiczenia/src/Firma.java
@@ -1,29 +1,38 @@
+import java.lang.reflect.Member;
+import java.util.Arrays;
+
+
+
 class Employee {             // klasa bazowa pracownik
     private String name;
     private String lastname;
     private String pesel;
     private double salary;
 
-    public Employee(String name, String lastname, String pesel, double salary) {     //konstruktor
+    public Employee(String name, String lastname, String pesel, double salary) {
         this.name = name;
         this.lastname = lastname;
         this.pesel = pesel;
         this.salary = salary;
     }
 
-    String getName() {
+    String getName()
+    {
         return (name);
     }
 
-    String getLastname() {
+    String getLastname()
+    {
         return (lastname);
     }
 
-    String getPesel() {
+    String getPesel()
+    {
         return (pesel);
     }
 
-    double getSalary() {
+    double getSalary()
+    {
         return (salary);
     }
 
@@ -42,12 +51,17 @@ class Employee {             // klasa bazowa pracownik
 class Boss extends Employee {
     private double bonus;
 
+    //Tworzymy nową klasę Boss, gdzie odwołuje się do klasy bazowej Employee i otrzymuje swoja prywatną zmienną.
+    //Konstruktor odwołuje się do klasy bazowej i pobiera zmienne stałe : name, lastname, pesel, salary.
+    //Funkcja super() służy do wywoływania konstruktorów klasy nadrzędnej (tej, z której dziedziczymy) o podanych parametrach,
+
     public Boss(String name, String lastname, String pesel, double salary) {
         super(name, lastname, pesel, salary);
         bonus = 0;
 
     }
         double getBonus()
+
         {
             return(bonus);
         }
@@ -58,7 +72,8 @@ class Boss extends Employee {
 
         }
 
-    public String toString(){
+    public String toString()
+    {
         return "Klasa Boss";
     }
 
@@ -67,32 +82,71 @@ class Boss extends Employee {
 class Manager extends Employee{
     private int overtime;
 
-    public Manager(String name, String lastname, String pesel, double salary){
+    public Manager(String name, String lastname, String pesel, double salary) {
         super(name, lastname, pesel, salary);
         overtime = 0;
-    }
 
-    double getOvertime()
+    }
+        double getOvertime ()
+
+        {
+            return (overtime);
+        }
+
+        public void setOvertime ( int o)
+        {
+            this.overtime = o;
+
+        }
+
+    public String toString()
     {
-        return(overtime);
-    }
-
-    public void setOvertime(int o)
-    {
-        this.overtime = o;
-
-    }
-    public String toString(){
         return "Klasa Menager";
     }
 
 
 }
 
-class Intern extends Employee {
-    public Intern(String name, String lastname, String pesel, double salary) {
-        super(name, lastname, pesel, salary);
+
+class Intern implements Comparable<Intern> {   //na potrzeby zadania 2
+
+    private String imie;
+
+    private String nazwisko;
+
+    private int iloscgodzin;
+
+    public Intern(String imie, String nazwisko, int iloscgodzin) {
+
+        this.imie = imie;
+
+        this.nazwisko = nazwisko;
+
+        this.iloscgodzin = iloscgodzin;
+
     }
+
+
+
+    public int compareTo(Intern obiekt) {
+
+        if(nazwisko.compareTo(obiekt.nazwisko) == 0)
+            if(imie.compareTo(obiekt.imie) == 0)
+                return iloscgodzin - obiekt.iloscgodzin;
+            else
+                return imie.compareTo(obiekt.imie);
+            else
+                return nazwisko.compareTo(obiekt.nazwisko);
+
+    }
+
+
+    public String toString() {
+
+        return (nazwisko + " " + imie + " " + iloscgodzin);
+
+    }
+
 }
 
 
@@ -100,27 +154,47 @@ public class Firma {
     public static void main(String args[]){
 
 
+        Intern[] interns = new Intern[6];
 
-        //test gita
+        interns[0] = new Intern("Marek", "Kowalski", 4);
+
+        interns[1] = new Intern("Jacek", "Kowalski", 3);
+
+        interns[2] = new Intern("Iwona", "Nowak", 8);
+
+        interns[3] = new Intern("Marta", "Nowak", 1);
+
+        interns[4] = new Intern("Adam", "Dynks", 2);
+
+        interns[5] = new Intern("Marek", "Kowal", 9);
+
+        Arrays.sort(interns);
+
+         for (Intern st: interns){
+
+            System.out.println(st);
+
+        }
+
+        Employee o1 = new Employee("Adrian", "Kowalski", "12345678985", 3500.65);
+        Employee o2 = new Boss("Andrzej", "Myć", "12345678996", 9000);
+        Employee o3 = new Manager("Tomasz", "Lis", "12345678996", 6000);
+
+        System.out.println(o1.toString());
+        System.out.println(o1.getName()+" "+o1.getLastname()+" "+o1.getPesel()+" "+o1.getSalary()+" zł ");
+
+        ((Boss) o2).setBonus(498.63);
+        System.out.println(o2.toString());
+        System.out.println(o2.getName()+" "+o2.getLastname()+" "+o2.getPesel()+" "+o2.getSalary()+" zł "+" + " + ((Boss) o2).getBonus()+" zł ");
 
 
-        Employee employee = new Employee("Adrian", "Kowalski", "12345678985", 3500.65);
-        System.out.println(employee.toString());
-        System.out.println(employee.getName()+" "+employee.getLastname()+" "+employee.getPesel()+" "+employee.getSalary()+" zł ");
-
-
-        Boss boss = new Boss("Andrzej","Myć", "12345678996", 9000);
-        boss.setBonus(498.63);
-        System.out.println(boss.toString());
-        System.out.println(boss.getName()+" "+boss.getLastname()+" "+boss.getPesel()+" "+boss.getSalary()+" zł "+" + " +boss.getBonus()+" zł ");
-
-        Manager manager = new Manager("Tomasz","Lis", "12345678996", 6000);
-        manager.setOvertime(5);
-        System.out.println(manager.toString());
-        System.out.println(manager.getName()+" "+manager.getLastname()+" "+manager.getPesel()+" "+manager.getSalary()+" zł "+manager.getOvertime()+"h ");
-
+        ((Manager) o3).setOvertime(5);
+        System.out.println(o3.toString());
+        System.out.println(o3.getName()+" "+o3.getLastname()+" "+o3.getPesel()+" "+o3.getSalary()+" zł "+ ((Manager) o3).getOvertime()+"h ");
 
     }
+
+ 
 }
 
 
